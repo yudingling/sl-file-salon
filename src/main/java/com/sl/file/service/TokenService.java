2e7c4219232dbf477e9279e29664b5fb338a3466
zuodan <yudingling@126.com>
2019-03-24 12:14:01 +0800

    update

--- src/main/java/com/sl/file/service/TokenService.java
+++ src/main/java/com/sl/file/service/TokenService.java
@@ -1,5 +1,7 @@
 package com.sl.file.service;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -12,11 +14,27 @@ public class TokenService {
 	@Value("${file.tokenRedisPrefix}")
 	private String tokeRedisPrefix;
 	
+	@Value("${file.userIdFieldInToken}")
+	private String userIdFieldInToken;
+	
 	@Autowired
 	private ObjectRedisApi objRedisApi;
 	
+	@SuppressWarnings("rawtypes")
 	public SToken getToken(String token){
 		String key = String.format("%s%s", this.tokeRedisPrefix, token);
-		return this.objRedisApi.get(key);
+		
+		Map tokenMap = this.objRedisApi.get(key);
+		
+		if(tokenMap != null){
+			Long uId = Long.parseLong(tokenMap.get(this.userIdFieldInToken).toString());
+			SToken tokenObj = new SToken();
+			tokenObj.setUserId(uId);
+			
+			return tokenObj;
+			
+		}else{
+			return null;
+		}
 	}
 }
