f75b6b7824df8f10b2e700ae1b4276dedcf81230
zuodan <yudingling@126.com>
2019-11-01 16:24:16 +0800

    update

--- src/main/java/com/sl/file/mapper/SlFileMapper.java
+++ src/main/java/com/sl/file/mapper/SlFileMapper.java
@@ -3,6 +3,6 @@ package com.sl.file.mapper;
 import com.sl.common.model.db.SlFile;
 import com.zeasn.common.mybatis.MyMapper;
 
-public interface SlFileMapper extends MyMapper<SlFile> {
+public interface SlFileMapper extends MyMapper<SlFile, SlFileMapper> {
 	
 }
